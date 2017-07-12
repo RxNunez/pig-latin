1f33ba6f8a4295078c74149ab72cfaa39a81b812
Ryan Nunez and Shyamal Punekar <shyamal@>
2017-07-12 15:41:42 -0700

    Include behavior 1-3 of non-alphabetic, single vowel, and word starting with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,56 @@
-var trans = function(translator){
-  var vowels = ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U'];
-  var consonants != ['a','e','i','o','u', 'A', 'E', 'I', 'O', 'U'];
+var translator = function(userInput){
+  var result = userInput;
+
+  // var userInput =userInput.split('');
 
 
+  if(!isNaN(userInput)){
+    alert("You have entered number");
+  }else if((userInput[0] === "a") || (userInput[0] === "e") || (userInput[0] === "i") || (userInput[0] === "o") || (userInput[0] === "u")){
+    // alert("Entered data contains vowel");
+    result = userInput + "way";
+  }else {
+    alert("not a cons");
 
+  }
+  return result;
+}
+
+
+var wordInput = function(userInput){
+  var answer=0;
+
+  userInput.map(function(word){
+    answer= word+  "way";
+    alert(answer);
+  });
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 $(document).ready(function() {
 
   $("form#pig-latin").submit(function(event) {
 
     event.preventDefault();
-    var translator = $("input#translator").val();
-    var wordInput = translator.split();
-    var display = trans(translator);
+
+    var blocks = $("input#translator").val();
+
+    var display = translator(blocks);
     $("#display").text(display);
   });
 });
