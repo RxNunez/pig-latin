0e750120c12a817a95a9e15cc7b2f891f4b0c705
Ryan Nunez and Shyamal Punekar <shyamal@>
2017-07-12 16:23:10 -0700

    Add behavior number 4- Single consonant word

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,24 @@
 var translator = function(userInput){
-  var result = userInput;
-
+var result = userInput;
   // var userInput =userInput.split('');
-
-
   if(!isNaN(userInput)){
     alert("You have entered number");
   }else if((userInput[0] === "a") || (userInput[0] === "e") || (userInput[0] === "i") || (userInput[0] === "o") || (userInput[0] === "u")){
     // alert("Entered data contains vowel");
-    result = userInput + "way";
+  result = userInput + "way";
   }else {
-    alert("not a cons");
-
+    alert(userInput.slice(1)+ userInput[0] + "ay");
+    // alert("not a cons");
   }
   return result;
 }
 
 
-var wordInput = function(userInput){
+var consonant = function(userInput){
+
   var answer=0;
 
-  userInput.map(function(word){
-    answer= word+  "way";
-    alert(answer);
-  });
+
 
 };
 
